500d7053df5e17affb9380f53325eaa854f50308
emna <emna.gaidii@gmail.com>
2024-01-29 10:37:23 +0100

    changes

--- src/app/components/artist-profile/artist-profile.component.ts
+++ src/app/components/artist-profile/artist-profile.component.ts
@@ -2,6 +2,9 @@ import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { BehaviorSubject, Observable, switchMap } from 'rxjs';
+import { Artist } from 'src/app/Models/Artist';
+import { Song } from 'src/app/Models/Song';
 import { ArtistProfileService } from 'src/app/services/artist-profile.service';
 
 @Component({
@@ -10,12 +13,8 @@ import { ArtistProfileService } from 'src/app/services/artist-profile.service';
   styleUrls: ['./artist-profile.component.css'],
 })
 export class ArtistProfileComponent implements OnInit {
-  artistId: string = '';
-  artist: any;
-  topTracks: any[] = [];
-  displayedTopTracks: any[] = [];
-  showMore: boolean = false;
-  moreInfo: string = '';
+  artist$!: Observable<Artist>;
+  topTracks$!: Observable<Song[]>;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,49 +22,13 @@ export class ArtistProfileComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      this.artistId = params['id'];
-      this.fetchArtistDetails();
-      this.fetchTopTracks();
-
-      /*
-      this.artistService.getArtistInfo(this.artist.href).subscribe(
-        (response) => {
-          console.log('Réponse de la fonction dans le service:', response);
-          // Traitez les données ici
-        },
-        (error) => {
-          console.error('Erreur lors de la fonction dans le service:', error);
-          // Traitez les erreurs ici
-        }
-      );*/
-    });
-  }
-
-  fetchArtistDetails(): void {
-    this.artistService.getArtistDetails(this.artistId).subscribe((data) => {
-      this.artist = data;
-    });
-  }
-
-  fetchTopTracks(): void {
-    this.artistService.getTopTracks(this.artistId).subscribe(
-      (data) => {
-        this.topTracks = data;
-        this.displayedTopTracks = this.topTracks.slice(0, 5);
-      },
-      (error) => {
-        // Gérez les erreurs ici selon vos besoins
-        console.error('Error fetching top tracks:', error);
-      }
+    this.artist$ = this.route.paramMap.pipe(
+      switchMap((params) =>
+        this.artistService.getArtistDetails(params.get('id'))
+      )
     );
-  }
-  toggleShowMore(): void {
-    this.showMore = !this.showMore;
-    if (this.showMore) {
-      this.displayedTopTracks = this.topTracks; // Show all tracks
-    } else {
-      this.displayedTopTracks = this.topTracks.slice(0, 5); // Show only the first 5 tracks
-    }
+    this.topTracks$ = this.route.paramMap.pipe(
+      switchMap((params) => this.artistService.getTopTracks(params.get('id')))
+    );
   }
 }
--- src/app/services/artist-profile.service.ts
+++ src/app/services/artist-profile.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { EMPTY, Observable, catchError, map } from 'rxjs';
 import { spotifyConfiguration } from 'src/config/constantes.config';
 import { Song } from '../Models/Song';
+import { SpotifyTrack } from '../SpotifyHelper';
+import { Artist } from '../Models/Artist';
 
 @Injectable({
   providedIn: 'root',
@@ -10,66 +12,23 @@ import { Song } from '../Models/Song';
 export class ArtistProfileService {
   private apiUrl = spotifyConfiguration.spotifyApiBaseUrl;
   constructor(private http: HttpClient) {}
-
-  /*
-  getArtistDetails(artistId: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/artists/${artistId}`);
-  }*/
-  getArtistDetails(artistId: string): Observable<any> {
+  getArtistDetails(artistId: string | null): Observable<any> {
     const url = `${this.apiUrl}/artists/${artistId}`;
 
     return this.http.get<any>(url);
   }
-  getArtistInfo(artistHref: string): Observable<any> {
-    const token = localStorage.getItem('access_token');
 
-    // Ajoutez le token à l'en-tête de la requête
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`,
-    });
-
-    // Effectuez la requête avec le token dans l'en-tête
-    return this.http.get(artistHref, { headers });
-  }
-
-  getTopTracks(artistId: string): Observable<Song[]> {
+  getTopTracks(artistId: string | null): Observable<Song[]> {
     const url = `${this.apiUrl}/artists/${artistId}/top-tracks?country=US`;
 
     return this.http.get<any>(url).pipe(
       map((response) =>
-        response.tracks.map((track: any) => this.convertToSong(track))
+        response.tracks.map((track: any) => SpotifyTrack(track))
       ),
       catchError((error) => {
-        // Gérez les erreurs ici selon vos besoins
         console.error('Error fetching top tracks:', error);
         return EMPTY;
       })
     );
-  }
-  private convertToSong(track: any): Song {
-    return {
-      id: track.id,
-      title: track.name,
-      artists: track.artists.map((artist: any) => ({
-        id: artist.id,
-        name: artist.name,
-      })),
-      album: {
-        id: track.album.id,
-        name: track.album.name,
-        imageUrl:
-          track.album.images.length > 0 ? track.album.images[0].url : undefined,
-      },
-      time: this.formatTime(track.duration_ms),
-      previewUrl: track.preview_url,
-      addedAt: track.added_at, // Vous devrez peut-être ajuster cela en fonction de votre réponse Spotify
-    };
-  }
-
-  // Ajoutez cette fonction pour formater la durée de la piste
-  private formatTime(duration_ms: number): string {
-    const minutes = Math.floor(duration_ms / 60000);
-    const seconds = ((duration_ms % 60000) / 1000).toFixed(0);
-    return `${minutes}:${+seconds < 10 ? '0' : ''}${seconds}`;
   }
 }
