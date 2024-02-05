a1f9b65b3660567562c7358ad6e6c6c8272ef8f6
Hamza-benAmmar <Hamza.benammar@insat.ucar.tn>
2024-02-05 13:41:47 +0100

    final touches

--- src/app/components/add-song/add-song.component.ts
+++ src/app/components/add-song/add-song.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Song } from 'src/app/Models/Song';
@@ -9,22 +9,21 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FormControl } from '@angular/forms';
 import {
+  catchError,
   debounceTime,
   distinctUntilChanged,
   Observable,
   of,
   switchMap,
+  tap,
 } from 'rxjs';
-
-
 
 @Component({
   selector: 'app-add-song',
   templateUrl: './add-song.component.html',
   styleUrls: ['./add-song.component.css'],
-
 })
-export class AddSongComponent implements OnInit ,OnChanges{
+export class AddSongComponent implements OnInit, OnChanges {
   searchIcon = faSearch;
   // @ts-ignore
   searchResults$: Observable<{ tracks: Song[] }>;
@@ -65,21 +64,25 @@ export class AddSongComponent implements OnInit ,OnChanges{
       uris: [song.uri],
       position: 0,
     };
+
     console.log(this.playlistId);
-    this.playlistService.AddItem(requestBody, this.playlistId).subscribe(
-      () => {
-        console.log('asaabiii');
-        this.toast.success('Added to the playlist');
-        this.spotifyService
-          .getPlaylistDetails(this.playlistId)
-          .subscribe((updatedDetails) => {
-            this.playlistService.updatePlaylistDetails(updatedDetails);
-          });
-      },
-      (error) => {
-        this.toast.error('Error adding item to the playlist');
-      }
-    );
+
+    this.playlistService
+      .AddItem(requestBody, this.playlistId)
+      .pipe(
+        switchMap(() => {
+          return this.spotifyService.getPlaylistDetails(this.playlistId);
+        }),
+        tap((updatedDetails) => {
+          this.playlistService.updatePlaylistDetails(updatedDetails);
+          this.toast.success('Added to the playlist');
+        }),
+        catchError((error) => {
+          this.toast.error('Error adding item to the playlist');
+          throw error; // Rethrow the error for further handling, if needed
+        })
+      )
+      .subscribe();
   }
   PlaySong(song: Song) {
     this.playerService.playMusic(song);
@@ -87,5 +90,4 @@ export class AddSongComponent implements OnInit ,OnChanges{
   private refreshData() {
     this.searchControl.setValue('');
   }
-
 }
